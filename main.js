614f162fc1072712c88c42e4d762ed43c47bbaf6
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-10-27 17:03:18 +0700

    Add files

---
+++ main.js
@@ -0,0 +1,39 @@
+const btn = document.getElementById("calculate");
+
+btn.addEventListener("click", function () {
+  let height = document.querySelector("#height").value;
+  let weight = document.querySelector("#weight").value;
+
+  if (height == "" || weight == "") {
+    alert("Please fill out the input fields!");
+    return;
+  }
+
+  // BMI = weight in KG / (height in m * height in m)
+
+  height = height / 100;
+
+  let BMI = weight / (height * height);
+
+  BMI = BMI.toFixed(2);
+
+  document.querySelector("#result").innerHTML = BMI;
+
+  let status = "";
+
+  if (BMI < 18.5) {
+    status = "Underweight";
+  }
+  if (BMI >= 18.5 && BMI < 25) {
+    status = "Healthy";
+  }
+  if (BMI >= 25 && BMI < 30) {
+    status = "Overweight";
+  }
+  if (BMI >= 30) {
+    status = "Obese";
+  }
+  document.querySelector(
+    ".comment"
+  ).innerHTML = `Result: You are <span id="comment">${status}</span>`;
+});
