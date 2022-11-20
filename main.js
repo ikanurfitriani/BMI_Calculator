ec40cd6a2a50be2778c87bdd8e4ed3a9df118370
ikanurfitriani <ikan07393@gmail.com>
2022-11-20 16:18:29 +0700

    Update code

--- main.js
+++ main.js
@@ -9,16 +9,10 @@ btn.addEventListener("click", function () {
     return;
   }
 
-  // BMI = weight in KG / (height in m * height in m)
-
   height = height / 100;
-
   let BMI = weight / (height * height);
-
   BMI = BMI.toFixed(2);
-
   document.querySelector("#result").innerHTML = BMI;
-
   let status = "";
 
   if (BMI < 18.5) {
